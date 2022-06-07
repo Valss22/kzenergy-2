ec26da67df70cf98f69abe42c00e0d12276e52ce
Valss22 <valsshokorov@gmail.com>
2022-06-07 18:39:25 +0600

    add test_successful_login_user

--- src/tests/test_user.py
+++ src/tests/test_user.py
@@ -1,10 +1,14 @@
+from typing import TypedDict
+
 import pytest
 from httpx import AsyncClient
+
+from src.app.user.model import User
 from src.app.user.types import Roles
 
 
 @pytest.fixture(scope="module")
-def user_req_body() -> dict:
+def user_setup() -> dict:
     login_req_body = {
         "email": "test@gmail.com",
         "password": "123"
@@ -17,12 +21,17 @@ def user_req_body() -> dict:
     })
     return {
         "register_req_body": register_req_body,
-        "login_req_body": login_req_body
+        "login_req_body": login_req_body,
+        "response_keys": [
+            "id", "role",
+            "fullname", "email",
+            "phone", "token"
+        ]
     }
 
 
-async def test_register_user(client: AsyncClient, user_req_body):
-    req_body = user_req_body["register_req_body"]
+async def test_register_user(client: AsyncClient, user_setup):
+    req_body = user_setup["register_req_body"]
     password = req_body["password"].encode()
     req_body.update({"password": password.decode()})
     response = await client.post(
@@ -30,12 +39,20 @@ async def test_register_user(client: AsyncClient, user_req_body):
         json=req_body
     )
     assert response.status_code == 200
-    assert list(response.json().keys()) == [
-        "id", "role",
-        "fullname", "email",
-        "phone", "token"
-    ]
+    assert list(response.json().keys()) == user_setup["response_keys"]
 
 
-async def test_login_user(client: AsyncClient, user_req_body):
-    pass
+async def test_successful_login_user(client: AsyncClient, user_req_body):
+    register_req_body = user_req_body["register_req_body"]
+    req_body = user_req_body["login_req_body"]
+    # password_hash = register_req_body["password"].encode()
+    # del register_req_body["password"]
+    # await User.create(
+    #     **register_req_body, password_hash=password_hash
+    # )
+    response = await client.post(
+        "/user/login/",
+        json=req_body
+    )
+    assert response.status_code == 200
+    assert list(response.json().keys()) == user_setup["response_keys"]
