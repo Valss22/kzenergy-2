14819973343307bdbe6ec0e73c2ba3911a03e1f6
Valss22 <valsshokorov@gmail.com>
2022-07-01 20:37:07 +0600

    Fix tests

--- src/app/waste/model.py
+++ src/app/waste/model.py
@@ -1,10 +1,10 @@
 from tortoise import fields, models
 
-from src.app.waste.types import TypesOfWaste
+from src.app.waste.types import AggregateState
 
 
 class Waste(models.Model):
     id = fields.UUIDField(pk=True)
     name = fields.CharField(max_length=50, unique=True)
-    type = fields.CharEnumField(TypesOfWaste)
+    type = fields.CharEnumField(AggregateState)
     density = fields.FloatField()
