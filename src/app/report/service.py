5d8757bf760c61a8d6f1174a6815f7443182a264
Valss22 <valsshokorov@gmail.com>
2022-07-09 18:14:54 +0600

    Fix create_report()

--- src/app/report/service.py
+++ src/app/report/service.py
@@ -8,7 +8,7 @@ class ReportService:
     async def create_report(self, facility_id: str, auth_header: str):
         user = await get_current_user(auth_header)
         report = await Report.create(user=user)
-        tickets = await Ticket.filter(
-            facility__id=facility_id,
+        await Ticket.filter(
+            facility_id=facility_id,
             archived=False
         ).update(report=report)
