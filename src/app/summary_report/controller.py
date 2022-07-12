c74228a2c9af94d1e537ac0662c804b6ab435d92
Valss22 <valsshokorov@gmail.com>
2022-07-12 16:12:05 +0600

    Add create_sum_report()

--- src/app/summary_report/controller.py
+++ src/app/summary_report/controller.py
@@ -1,9 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Header, Depends
+
+from src.app.summary_report.service import SummaryReportService
 
 summary_report = APIRouter()
 SUMMARY_REPORT_ENDPOINT = "/summary/"
 
 
 @summary_report.post(SUMMARY_REPORT_ENDPOINT)
-async def create_sum_report():
-    pass
+async def create_sum_report(
+    Authorization: str = Header(),
+    summary_report_service: SummaryReportService = Depends()
+):
+    return await summary_report_service.create_sum_report(Authorization)
--- src/app/summary_report/service.py
+++ src/app/summary_report/service.py
@@ -1,4 +1,13 @@
-class SummaryReportService:
+from src.app.report.model import Report
+from src.app.summary_report.model import SummaryReport
+from src.app.user.service import get_current_user
 
-    async def create_sum_report(self):
+
+class SummaryReportService:
+    async def get_facilities_with_reports(self) -> dict[str, list]:
         pass
+
+    async def create_sum_report(self, auth_header: str):
+        user = await get_current_user(auth_header)
+        summary_report = await SummaryReport.create(user=user)
+        await Report.filter(archived=False).update(summaryReport=summary_report)
