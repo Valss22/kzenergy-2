d0ab80afd264e5aae7da3f1fb03865a04016bce6
Valss22 <valsshokorov@gmail.com>
2022-07-12 16:49:47 +0600

    Add arhcived filter in create_sum_report()

--- src/app/routers.py
+++ src/app/routers.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from src.app.facility.controller import facility_router
 from src.app.report.controller import report_router
+from src.app.summary_report.controller import summary_report_router
 from src.app.ticket.controller import ticket_router
 from src.app.user.controller import user_router
 
@@ -10,3 +11,4 @@ api_router.include_router(user_router)
 api_router.include_router(facility_router)
 api_router.include_router(ticket_router)
 api_router.include_router(report_router)
+api_router.include_router(summary_report_router)
--- src/app/summary_report/controller.py
+++ src/app/summary_report/controller.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Header, Depends
 
 from src.app.summary_report.service import SummaryReportService
 
-summary_report = APIRouter()
+summary_report_router = APIRouter()
 SUMMARY_REPORT_ENDPOINT = "/summary/"
 
 
-@summary_report.post(SUMMARY_REPORT_ENDPOINT)
+@summary_report_router.post(SUMMARY_REPORT_ENDPOINT)
 async def create_sum_report(
     Authorization: str = Header(),
     summary_report_service: SummaryReportService = Depends()
