083d64ec1da04f1fc5d29ef525c37d3871d71a70
Valss22 <valsshokorov@gmail.com>
2022-07-12 16:42:54 +0600

    Add arhcived filter in create_sum_report()

--- src/app/summary_report/service.py
+++ src/app/summary_report/service.py
@@ -1,5 +1,7 @@
 from src.app.report.model import Report
 from src.app.summary_report.model import SummaryReport
+from src.app.ticket.model import Ticket
+from src.app.ticket.types import TicketStatus
 from src.app.user.service import get_current_user
 
 
@@ -11,3 +13,7 @@ class SummaryReportService:
         user = await get_current_user(auth_header)
         summary_report = await SummaryReport.create(user=user)
         await Report.filter(archived=False).update(summaryReport=summary_report)
+        await Report.filter(summaryReport=summary_report).update(archived=True)
+        await Ticket.filter(
+            archived=False, status=TicketStatus.ACCEPTED.value
+        ).update(archived=True)
