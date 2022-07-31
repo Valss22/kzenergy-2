3af8ddc84313ed0f3a90fca64c67f267f049cec6
Valss22 <valsshokorov@gmail.com>
2022-07-31 14:26:05 +0600

    Add filter on status in get_wastes_quantity()

--- src/app/waste/service.py
+++ src/app/waste/service.py
@@ -1,5 +1,6 @@
 from src.app.summary_report.service import QUANTITY_BY_MEASURE
 from src.app.ticket.model import Ticket
+from src.app.ticket.types import TicketStatus
 from src.app.waste.model import Waste
 
 
@@ -9,7 +10,10 @@ class WasteService:
         wastes: list[dict] = []
         for waste in await Waste.all():
             quantity = {**QUANTITY_BY_MEASURE}
-            for ticket in await Ticket.filter(wasteName=waste.name, archived=False):
+            for ticket in await Ticket.filter(
+                wasteName=waste.name, archived=False,
+                status=TicketStatus.ACCEPTED.value
+            ):
                 quantity.update({ticket.measureSystem: quantity[ticket.measureSystem] + ticket.quantity})
             wastes.append({"id": waste.id, "name": waste.name, "quantity": quantity})
         return wastes
