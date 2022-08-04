e0213a5c806a85cb7f06ea59046e751489de6463
Valss22 <valsshokorov@gmail.com>
2022-08-04 15:36:19 +0600

    Fix

--- src/app/chart/service.py
+++ src/app/chart/service.py
@@ -15,16 +15,15 @@ class ChartService:
 
     async def get_lineplot(self):
         repsonse = {}
-        for sum_report in await SummaryReport.all():
-            info: list[dict] = []
-            for waste in await Waste.all():
-                if waste.name in repsonse.keys():
-                    continue
+
+        for waste in await Waste.all():
+            for sum_report in await SummaryReport.all():
+                info: list[dict] = []
                 qnt_by_measure = {**QUANTITY_BY_MEASURE}
                 for ticket in await Ticket.filter(wasteName=waste.name):
                     qnt_by_measure.update({
                         ticket.measureSystem: qnt_by_measure[ticket.measureSystem] + ticket.quantity
                     })
-            info.append({**qnt_by_measure, "date": sum_report.date})
-            repsonse.update({waste.name: {"limit": LIMIT[waste.name], "info": info}})
+                info.append({**qnt_by_measure, "date": sum_report.date})
+                repsonse.update({waste.name: {"limit": LIMIT[waste.name], "info": info}})
         return repsonse
