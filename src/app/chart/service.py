01c8c3144de7e8c89169e9a2439fcd8b30db1e23
Valss22 <valsshokorov@gmail.com>
2022-08-04 14:47:45 +0600

    Fix lineplot

--- src/app/chart/service.py
+++ src/app/chart/service.py
@@ -16,10 +16,10 @@ class ChartService:
     async def get_lineplot(self):
         repsonse = {}
         for sum_report in await SummaryReport.all():
+            info: list[dict] = []
             for waste in await Waste.all():
                 if waste.name in repsonse.keys():
                     continue
-                info: list[dict] = []
                 qnt_by_measure = {**QUANTITY_BY_MEASURE}
                 for ticket in await Ticket.filter(wasteName=waste.name):
                     qnt_by_measure.update({
